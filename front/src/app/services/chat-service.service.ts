16175f8472082432d2d84dcd88ac21b56761ab09
Keijur0 <tran.qlam@gmail.com>
2024-09-16 15:56:46 +0200

    [Front] ChatService

---
+++ front/src/app/services/chat-service.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { Client, Message } from '@stomp/stompjs';
+import { Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChatServiceService {
+
+  private stompClient: Client | undefined;
+
+  private messageSubject: Subject<string> = new Subject();
+
+  constructor() {
+    this.initializeWebSocketConnection();
+  }
+
+  private initializeWebSocketConnection() {
+    const socket = new WebSocket('http://localhost:8080/chat-websocket');
+    this.stompClient = new Client({
+      webSocketFactory: () => socket
+    });
+
+    this.stompClient.onConnect = () => {
+      this.stompClient?.subscribe('/topic/messages', (message: Message) => {
+        this.messageSubject.next(message.body);
+      });
+    };
+
+    this.stompClient.activate();
+  }
+
+  public sendMessage(chatMessage: string) {
+    this.stompClient?.publish({
+      destination: '/app/sendMessage',
+      body: JSON.stringify(chatMessage)
+    });
+  }
+
+  public getMessages() {
+    return this.messageSubject.asObservable();
+  }
+
+}
