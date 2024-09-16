74806184cbfbc9feea368b129f150214c30d1fa0
Keijur0 <tran.qlam@gmail.com>
2024-09-16 15:39:09 +0200

    [Back] ChatController

--- back/src/main/java/com/yourcaryourway/pocchat/controller/ChatController.java
+++ back/src/main/java/com/yourcaryourway/pocchat/controller/ChatController.java
@@ -1,5 +1,25 @@
 package com.yourcaryourway.pocchat.controller;
 
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.yourcaryourway.pocchat.model.ChatMessage;
+import com.yourcaryourway.pocchat.service.ChatService;
+
+@RestController
+@RequestMapping("/api/chat")
 public class ChatController {
 
+    private final ChatService chatService;
+
+    public ChatController(ChatService chatService) {
+        this.chatService = chatService;
+    }
+
+    @MessageMapping("/sendMessage")
+    public void sendMessage(@Payload ChatMessage chatMessage) {
+        chatService.sendMessage(chatMessage);
+    }
 }
